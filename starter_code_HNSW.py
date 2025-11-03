bee2b8dbcb7c94316348f7d60e62056b0b49f0bb
T-I-C-E2002 <bartukoksal@hotmail.com>
2025-11-02 21:45:38 -0600

    Starter code added!

--- starter_code_HNSW.py
+++ starter_code_HNSW.py
@@ -10,7 +10,36 @@ def evaluate_hnsw():
     # download data, build index, run query
 
     # write the indices of the 10 approximate nearest neighbours in output.txt, separated by new line in the same directory
+    with h5py.File('/home/bartu_koksal/cs511-mp2/FA2025-CS511-MP2-hnsw/sift-128-euclidean.hdf5', "r") as f:
+        print(list(f.keys()))
+        xb = f['train'][:] #shape : (1000000, 128)
+        #print(xb.shape)
+        xq = f['test'][:] #shape : (10000, 128)
+        #print(xq.shape)
+        gt = f['neighbors'][:] #shape : (10000, 100) indices into train
+        #print(gt.shape)
     
+    M=16
+    index = faiss.IndexHNSWFlat(xb.shape[1], M, faiss.METRIC_L2)
+    index.hnsw.efConstruction = 200
+    index.hnsw.efSearch = 200
+
+    q = xq[:1] #first query from test embeddigns
+
+    if xb.dtype != np.float32:
+        xb = xb.astype(np.float32, copy=False)
+    if q.dtype != np.float32:
+        q = q.astype(np.float32, copy=False)
+    
+    index.add(xb)
+    D, I = index.search(q, 10) #search for 10 nearest neighbours
+    top10_indices = I[0].tolist() #indices of 10 nearest neighbours for the first query
+    out_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output.txt")
+    with open(out_path, "w") as f:
+        for idx in top10_indices:
+            f.write(f"{idx}\n")
+    print(f"Wrote top-10 ANN indices for the first query to {out_path}")
+
 
 if __name__ == "__main__":
     evaluate_hnsw()
